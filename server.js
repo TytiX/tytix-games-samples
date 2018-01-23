6f35def58da56520a326d2416c6bfdab37ba93da
Belliard Antoine <antoine.belliard@soprasteria.com>
2018-01-23 13:51:28 +0100

    run express http base

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const pjson = require('package.json');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world running version '+pjson.version+'\n');
+});
+
+app.listen(PORT, HOST);
